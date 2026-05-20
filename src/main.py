4f9c2dca021fb85e1b68a94ae29a5115d9113d9c
KiraboKarynaKiwagama <k.kiwagama@alustudent.com>
2026-05-20 20:39:36 +0200

    Updated Python file

--- src/main.py
+++ src/main.py
@@ -2,10 +2,6 @@ import re
 import json
 import os
 
-
-
-
-
 with open("input/raw-text.txt", "r") as file:
     raw_text = file.read()
 
@@ -23,17 +19,18 @@ patterns = {
     # 4. URL
     "url_pattern": r"https?://[^\s]+",
    
-    # 5. Hashtag (Simplified to grab # followed by any alphanumeric/underscore character)
+    # 5. Hashtag 
     "hashtag_pattern": r"#[a-zA-Z0-9_]+"
 }
 
 
-def mask_credit_card(card_number):
+#now we will mask the credit card numbers by relacing all but the last 4 digits with asterisks. We will also remove any spaces or dashes from the credit card number before masking.
+def mask_credit_card(card_number): 
     digits_only = re.sub(r'[\s\-]', '', card_number)
     masked = '*' * (len(digits_only) - 4) + digits_only[-4:]
     return masked
 
-
+# Now we will loop through the patterns, find matches in the raw text, and store the results in a dictionary. For credit card numbers, we will mask them before storing.
 results = {}
 
 
@@ -49,6 +46,7 @@ for label, pattern in patterns.items():
 with open("output/sample_output.json", "w") as output_file:
     json.dump(results, output_file, indent=4)
        
+#Now we will print a message to the console indicating that the results have been saved to the output file.
 print("Results have been saved to output/sample_output.json")
 
 
